677f4cf78da75b4b08c0801b83c995e9c7282e50
themehakcodes <codewithmehak@gmail.com>
2024-09-01 15:04:41 +0530

    upwload

--- portfolio/data/index.ts
+++ portfolio/data/index.ts
@@ -41,32 +41,37 @@ export const gridItems = [
     title: "Tech enthusiast with a passion for development.",
     description: "",
     className: "lg:col-span-3 md:col-span-3 md:row-span-1",
-    imgClassName: "",
+    imgClassName: "w-full h-full",
     titleClassName: "justify-start",
     img: "/grid.svg",
     spareImg: "/b4.svg",
   },
-
   {
-    id: 5,
+    id:56,
     title: "Currently building a JS Animation library",
     description: "The Inside Scoop",
-    className: "md:col-span-3 md:row-span-2",
+    className: "md:col-span-1 md:row-span-1",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
     img: "/b5.svg",
     spareImg: "/grid.svg",
   },
+ 
   {
     id: 6,
     title: "Do you want to start a project together?",
     description: "",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    className: "lg:col-span-1  md:row-span-1",
     imgClassName: "",
     titleClassName: "justify-center md:max-w-full max-w-60 text-center",
     img: "",
     spareImg: "",
   },
+
+ 
+  
+
+ 
 ];
 
 export const projects = [
