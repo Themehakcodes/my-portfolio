4810237ff8849fbef42cbb1de5557e659b3edede
themehakcodes <themehakcodes.tech@gmail.com>
2024-09-01 11:36:49 +0530

    new commit

--- portfolio/components/Hero.tsx
+++ portfolio/components/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Spotlight } from './ui/Sportlight'; 
 import { TextGenerateEffect } from './ui/text-generate-effect';
 import MagicButton from './ui/MagicButton';
+import { FaLocationArrow } from 'react-icons/fa';
 
 const Hero = () => {
   return (
@@ -11,7 +12,7 @@ const Hero = () => {
         <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill='purple' />            
         <Spotlight className='top-28 left-80 h-[80vh] w-[50vw]' fill='blue' />            
       </div>
-      <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.3] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+      <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.2] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
       <div className='flex justify-center relative my-20 z-10'>
@@ -32,6 +33,8 @@ const Hero = () => {
           <a href='#about'>
             <MagicButton 
             title="Show my work"
+            icon={<FaLocationArrow />}
+            position='left'
             />
           </a>
         </div>
